69c106e4725abe29c4bf669aeaaa2ce04b0319f1
IntelRadeon266 <89801608+IntelRadeon266@users.noreply.github.com>
2023-05-21 20:36:26 +0300

    Update resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -43,7 +43,7 @@ const Resume: NextPage = () => {
           <h5 className="my-2 text-2xl font-bold ">Experience</h5>
           <div>
             <h5 className="my-1 text-xl font-bold">Internship</h5>
-            <p className="font-semibold">ATTACHEE, Ministry of Tourism(August 2022- December 2022)</p>
+            <p className="font-semibold">Ministry of Tourism(August 2022- December 2022)</p>
             <h5 className="my-1 text-xl font-bold">Attache</h5>
             <p className="font-semibold">Jambo Travel House Limited(May 2018- August 2018)</p>
             <h5 className="my-1 text-xl font-bold">Data Entry</h5>
